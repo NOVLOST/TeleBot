a19df15a7d43dd305e750c7f88a24a9c5c0d2355
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2024-11-22 20:44:36 +0400

    Update handler.py

--- app/handler.py
+++ app/handler.py
@@ -1,4 +1,4 @@
-from aiogram import F, Router
+from aiogram import F, Router,Bot
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
@@ -9,21 +9,13 @@ import classes.client_class as cl
 
 
 router = Router()
-
+bot = Bot(token='7962043379:AAGXTLRJIlnnDG0nfKHbrGmCkQ_FWo8zdYQ')
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     cl.client.full_name = message.from_user.full_name  # Полное имя пользователя
 
     await message.answer(f"Здравствуйте, {cl.client.full_name}\nЧем я могу вам помочь?", reply_markup=kb.start_keyboard)
-
-@router.message(F.photo)
-async def photo_handler(message: Message) -> None:
-    photo_data = message.photo[0]
-    photo_id = message.photo[0].file_id
-    await message.answer(f"{photo_data}")  # получаем id файла(фотографии)
-    # отправляем пользователю фотографию по id
-    await message.answer_photo(photo=f'{photo_id}')
 
 
 @router.message(F.text == 'FAQ ❔')
@@ -36,41 +28,95 @@ async def cmd_help(message: Message):
 
 @router.message(F.text == "Создать новую заявку 📝")
 async def registration(message: Message, state: FSMContext):
+    await state.update_data(id_account = message.from_user.id)
     await state.set_state(cl.client.real_first_name)
     await message.answer('Введите ваше имя')
 
 @router.message(cl.client.real_first_name)
 async def reg_real_first_name(message: Message, state: FSMContext):
-    await state.update_data(real_first_name = message.text)
-    print(cl.client.real_first_name)
+    await state.update_data(real_first_name = message.text )
+    data = await state.get_data()
+    print(f'{data['real_first_name']}')
     await state.set_state(cl.client.real_second_name)
     await message.answer("Введите вашу фамилию")
 
 @router.message(cl.client.real_second_name)
 async def reg_real_second_name(message: Message, state: FSMContext):
-    await state.update_data(real_second_name = message.text)
+    await state.update_data(real_second_name=message.text)
     await state.set_state(cl.client.real_third_name)
     await message.answer("Введите ваше отчество")
 
 @router.message(cl.client.real_third_name)
 async def reg_real_third_name(message: Message, state: FSMContext):
-    await state.update_data(real_third_name = message.text)
+    await state.update_data(real_third_name=message.text)
     await state.set_state(cl.client.school_or_student)
     await message.answer("Вы школьник или студент?",reply_markup=kb.school_of_student)
 
-@router.message(F.data == 'student')
-async def reg_student(callback: CallbackQuery, state: FSMContext):
-    await state.update_data(school_or_student = 'student')
-    print("stud")
+@router.message(F.text == 'Студент')
+async def reg_student(message: Message, state: FSMContext):
+    await state.update_data(school_or_student = 'студент')
     await state.set_state(cl.client.city)
-    await callback.answer("Введите ваш город проживания")
+    await message.answer("Введите ваш город проживания")
 
-@router.message(F.data == 'school')
-async def reg_school(callback: CallbackQuery, state: FSMContext):
-    await callback.answer('[jhjoj')
-    await state.update_data(school_or_student = 'school')
-    print("scholl")
+@router.message(F.text == 'Школьник')
+async def reg_school(message: Message, state: FSMContext):
+    await state.update_data(school_or_student = 'школьник')
     await state.set_state(cl.client.city)
-    await callback.message.answer("Введите ваш город проживания")
+    await message.answer("Введите ваш город проживания")
+
+@router.message(cl.client.city)
+async def reg_city(message: Message, state: FSMContext):
+    await state.update_data(city = message.text)
+    await state.set_state(cl.client.education_place)
+    await message.answer("Введите ваше место обучение")
+
+@router.message(cl.client.education_place)
+async def reg_education_place(message: Message, state: FSMContext):
+    await state.update_data(education_place = message.text)
+    await state.set_state(cl.client.phone_number)
+    await message.answer("Отправьте ваш номер телефона",reply_markup=kb.get_number)
+
+@router.message(cl.client.phone_number,F.contact)
+async def reg_number(message: Message, state: FSMContext):
+    await state.update_data(phone_number = message.contact.phone_number)
+    await state.set_state(cl.client.age)
+    await message.answer("Введите ваш возраст")
+
+@router.message(cl.client.age)
+async def reg_age(message: Message, state: FSMContext):
+    try:
+        if int(message.text) < 30:
+            await state.update_data(age = message.text)
+            await state.set_state(cl.client.type_of_work)
+            await message.answer("Выберите тип вашей работы", reply_markup=kb.choice_type_of_work)
+        else:
+            await message.answer("Введите верный возраст!")
+    except:
+        await message.answer("Введите верный возраст!")
+
+@router.message(F.text)
+async def reg_work(message: Message, state: FSMContext):
+    await state.update_data(type_of_work = message.text)
+    await state.set_state(cl.client.id_file)
+    await message.answer("Отправте вашу работу")
+
+@router.message(F.photo)
+async def photo_handler(message : Message,state: FSMContext):
+    photo = message.photo[-1]
+    photo_id = photo.file_id
+    file_info = await bot.get_file(photo_id)
+    data = await state.get_data()
+    await bot.download_file(file_info.file_path,f'{data["real_first_name"]} {data["real_second_name"]} {data["id_account"]}.jpg')
+    await message.answer(f"имя: {data["real_first_name"]}\n"
+                         f" фамилия: {data['real_second_name']}\n"
+                         f" отчество: {data['real_third_name']}\n"
+                         f" возраст: {data['age']}\n"
+                         f" статус: {data['school_or_student']}\n"
+                         f" город: {data["city"]}\n"
+                         f" место обучения: {data["education_place"]}\n"
+                         f" телефон: {data['phone_number']}\n"
+                         f" тип работы: {data["type_of_work"]}")
+    await message.answer("Все верно? если допустили ошибку попроси меня для редактирования заявки ;)")
+    await state.clear()
 
 
