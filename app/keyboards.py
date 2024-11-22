23ff0b728ea541399f54205ffeb1fa1e81cafd5e
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2024-11-22 20:44:57 +0400

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,14 +1,9 @@
-from gc import callbacks
 
 from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardButton,InlineKeyboardMarkup)
 
 
-# main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='регистрация')],
-#                                     [KeyboardButton(text="FAQ")],
-#                                     [KeyboardButton(text='Ваши заявки')]],
-#                            resize_keyboard=True,
-#                            input_field_placeholder="Выберите пункт меню...")
+
 
 # Кнопки
 start_keyboard = ReplyKeyboardMarkup(
@@ -17,22 +12,51 @@ start_keyboard = ReplyKeyboardMarkup(
             KeyboardButton(text="Создать новую заявку 📝"),
             KeyboardButton(text="Редактировать заявку ✏️"),
         ],
-        {
+        [
             KeyboardButton(text="Профиль 👤"),
             KeyboardButton(text="FAQ ❔" ),
-        }
+        ]
     ],
     resize_keyboard=True,
     input_field_placeholder="Что вас интересует?"
 )
 
-#inline кнопки
-school_of_student = InlineKeyboardMarkup(
-    inline_keyboard=[
+
+school_of_student = ReplyKeyboardMarkup(
+    keyboard=[
     [
-        InlineKeyboardButton(text = "Студент",callback_data = 'student'),
-        InlineKeyboardButton(text = "Школьник",callback_data = "school" )
+        KeyboardButton(text = "Студент"),
+        KeyboardButton(text = "Школьник")
     ]
 
-    ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите один вариант"
+)
+
+
+get_number = ReplyKeyboardMarkup(
+    keyboard = [
+        [
+            KeyboardButton(text = 'Отправить номер',request_contact=True )
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="чтобы отправить номер нажните на кнопку"
+)
+
+choice_type_of_work = ReplyKeyboardMarkup(
+    keyboard = [
+        [
+            KeyboardButton(text = 'Видео')
+        ],
+        [
+            KeyboardButton(text = "Фото")
+        ],
+        [
+            KeyboardButton(text = 'Другое')
+        ]
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Выберите один из вариантов"
 )
