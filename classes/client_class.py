f009ff2586689efc8947e06648905bb52a1c3a1c
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2025-02-22 15:33:24 +0400

    Update client_class.py

--- classes/client_class.py
+++ classes/client_class.py
@@ -3,24 +3,24 @@ from aiogram.fsm.state import State, StatesGroup
 
 class User(StatesGroup):
 
-        real_first_name = State()
-        real_second_name = State()
-        real_third_name = State()
-        school_or_student = State()
-        city = State()
-        education_place = State()
-        phone_number = State()
-        type_of_work = State()
-        age = State()
-        id_file = State()
-        id_account = State()
-        redux_mod = State()
-        redux_mod_2 = State()
-        redux_mod_3 = State()
-        num_of_photo = 0
-        num_of_bid = 0
-        bid_for_redux = []
-        dict_redux_bid = {}
+        real_first_name = State() #имя
+        real_second_name = State() #фамилия
+        real_third_name = State()# отчество
+        school_or_student = State()#статус учашегося
+        city = State() # город проживания
+        education_place = State() # место обучения
+        phone_number = State() #номер телефона
+        type_of_work = State() # тип работы
+        age = State() # возраст
+        id_file = State() #мб удалим
+        id_account = State()# id акка телеграм
+        redux_mod = State()# вывод в чат всех заявок и выбор интересующей
+        redux_mod_2 = State()# выбор пункта для редактирования заявки
+        redux_mod_3 = State()# сохранение изменений и вывод об успешной операции
+        num_of_photo = 0#счетчик номера фотографии
+        num_of_bid = 0#счетчик номера заявки
+        dict_redux_bid = {} #словарь для сохранения номеров заявок для редактирования
+        #пользователь пишет номер это ключ для поиска нужной заявки
 
 
 client = User()
